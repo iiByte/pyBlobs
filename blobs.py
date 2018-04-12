5c894a06ba56dccbdd12ed549ead62fe14cd3dce
Stephane Gagnon <stephane.gagnon00@gmail.com>
2018-04-12 11:52:37 -0400

    Initial

---
+++ blobs.py
@@ -0,0 +1,14 @@
+from colorama import Fore, Style
+from libapple import iDevice
+
+device = iDevice(input("Device Identifier: "))
+firmwareList = list(device.signed.keys())
+
+for i in firmwareList:
+	if device.signed[i] == True:
+		print(Fore.GREEN + i)
+	else:
+		print(Fore.RED + i)
+print(Style.RESET_ALL)
+
+print(device)
---
+++ libapple.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen, Request
+import json
+from colorama import Fore
+import os
+
+class iDevice():
+	def __init__(self, indentifier):
+		self.indentifier = indentifier
+		self.ipswURL = "https://api.ipsw.me/v4/device/{}?type=ipsw".format(indentifier)
+		print("Requesting for {}...".format(indentifier))
+		self.HTTPSReq = Request(self.ipswURL, headers={'Accept': 'application/json'})
+		print("Got response.")
+		self.JSONlist = json.loads(urlopen(self.HTTPSReq).read())
+		self.firmwares = self.JSONlist["firmwares"]
+		self.signed = {}
+
+		for ver in self.firmwares:
+			curFirmware = ver["version"]
+			curSigned = ver["signed"]
+			self.signed[curFirmware] = curSigned
+
+	def __str__(self):
+		
+		columns = int(os.popen('stty size', 'r').read().split()[1])
+		padding = [(columns//2)-6,(columns - columns//2)-5]
+		line1 = "-"*padding[0]+"Device Info"+"-"*padding[1]
+		line2 = "Name:{:>75}".format(self.JSONlist["name"])
+		line3 = "Indentifier:{:>68}".format(self.JSONlist["identifier"])
+		line4 = "Board Config:{:>67}".format(self.JSONlist["boardconfig"])
+		line5 = "Platform:{:>71}".format(self.JSONlist["platform"])
+		line6 = "-"*columns
+
+		returnVal = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5 + "\n" + line6+ "\n" 
+		return returnVal
